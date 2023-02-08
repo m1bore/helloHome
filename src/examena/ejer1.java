7500b58521bb38db7904a1709d55d2d5067fbe04
Miles Bosch <milbosrej@alu.edu.gva.es>
2023-02-08 09:18:20 +0100

    Arreglao

--- src/examena/ejer1.java
+++ src/examena/ejer1.java
@@ -13,8 +13,8 @@ public class ejer1 {
 		System.out.println("Write the height of the cylinder:");
 		int h=k.nextInt();k.nextLine();
 		if (r>0 && h>0) {
-			System.out.println("Erro:");
-			System.out.println("Bluewaffle");
+			System.out.println("Erro: the height and the radius must be positive");
+
 		}
 		
 			
